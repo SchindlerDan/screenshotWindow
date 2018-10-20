601f842c5d4b2c2b8ffae3fa09f27e68628f0597
Javier Ramirez <ramijavi@mail.gvsu.edu>
2018-10-20 19:55:59 -0400

    Updated Screenshot.java
    The code will now take a screenshot and create a window with no border or buttons every 3 seconds

--- Screenshot.java
+++ Screenshot.java
@@ -5,16 +5,36 @@ import java.awt.Robot;
 import java.awt.image.BufferedImage; 
 import java.io.IOException; 
 import java.io.File; 
-import javax.imageio.ImageIO; 
-import java.awt.*;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 public class Screenshot extends JPanel{ 
+	
     public static final long serialVersionUID = 1L; 
+    
     public static void main(String[] args) 
     { 
-        try { 
-            Thread.sleep(5000); 
+    	/*
+    	 * This for-loop calls the methods for taking a screenshot and
+    	 * creating a window every 3 seconds 
+    	 */
+    	for(int i=0; i < 500; i++) {
+    		try {
+    			Thread.sleep(3000); 
+        		takeScreenshot();
+        		createWindow();
+    		} catch (Exception e) {
+    			System.out.print("Error Detected");
+    		}
+    	}
+    } 
+
+    /*
+     * Method for taking screenshot of the current screen 
+     * and saving the image as a file
+     */
+    private static void takeScreenshot() {
+    	try { 
             Robot r = new Robot(); 
   
             // It saves screenshot to desired path 
@@ -27,37 +47,34 @@ public class Screenshot extends JPanel{
             ImageIO.write(Image, "jpg", new File(path)); 
             System.out.println("Screenshot saved"); 
         } 
-        catch (AWTException | IOException | InterruptedException ex) { 
+        catch (AWTException | IOException ex) { 
             System.out.println(ex); 
         }
-	//
-	createWindow();	
+    }
 
 
-
-
-    } 
-
-
-
+    /*
+     * Method for creating a window that displays the previously
+     * taken screenshot
+     */
     private static void createWindow(){
 	//img is the screenshot we took.
 	//BufferedImage img = new BufferedImage(0,0,TYPE_INT_ARGB);
 	try{    
 		BufferedImage img = ImageIO.read(new File("screenshot.jpg"));
 		JFrame jf = new JFrame("");
+		jf.setUndecorated(true);
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		jf.add(new JLabel(new ImageIcon(img)));
 		jf.setVisible(true);
 		jf.setExtendedState(jf.getExtendedState() | JFrame.MAXIMIZED_BOTH);
-	
-	
-	
 	}
 	catch(Exception e){
 		System.out.println("error finding screenshot");
 		System.exit(1);
 	}
+	
+	
 //	JFrame jf = new JFrame("");
 //	jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 //	jf.add(new JLabel(new ImageIcon(img)));
@@ -66,4 +83,5 @@ public class Screenshot extends JPanel{
 	
     }
 }
+
 
